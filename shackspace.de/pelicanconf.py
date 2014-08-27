2836fe09010a10ea9acf3e0fca9acbf85ca7996f
hdznrrd <github@follvalsch.de>
2014-08-27 19:03:04 +0200

    folder structure and siteurl

--- shackspace.de/pelicanconf.py
+++ shackspace.de/pelicanconf.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = u'shack e.V.'
 SITENAME = u'shackspace - Der Hackerspace in Stuttgart'
 SITEURL = ''
+#RELATIVE_URLS = True
 
 PATH = 'content'
 
@@ -34,3 +35,39 @@ DEFAULT_PAGINATION = 6
 
 DEFAULT_CATEGORY = 'misc'
 USE_FOLDER_AS_CATEGORY = True
+
+THEME = 'themes/pelican-bootstrap3'
+
+ARTICLE_URL =           'b/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS =       'b/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+ARTICLE_LANG_URL =      'b/{date:%Y}/{date:%b}/{date:%d}/{slug}-{lang}/'
+ARTICLE_LANG_SAVE_AS =  'b/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+
+DRAFT_URL =             'd/{slug}/'
+DRAFT_SAVE_AS =         'd/{slug}/index.html'
+DRAFT_LANG_URL =        'd/{slug}-{lang}/'
+DRAFT_LANG_SAVE_AS =    'd/{slug}-{lang}/index.html'
+
+PAGE_URL =              'p/{slug}/'
+PAGE_SAVE_AS =          'p/{slug}/index.html'
+PAGE_LANG_URL =         'p/{slug}-{lang}/'
+PAGE_LANG_SAVE_AS =     'p/{slug}-{lang}/index.html'
+
+CATEGORY_URL =          'cat/{slug}/'
+CATEGORY_SAVE_AS =      'cat/{slug}/index.html'
+TAG_URL =               'tag/{slug}/'
+TAG_SAVE_AS =           'tag/{slug}/index.html'
+AUTHOR_URL =            'by/{slug}/'
+AUTHOR_SAVE_AS =        'by/{slug}/index.html'
+
+TEMPLATE_PAGES = {      'article_list.html':    'b/index.html' }
+
+
+#PAGINATION_PATTERNS = (
+#    (1, '{base_name}/', '{base_name}/index.html'),
+#    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+#)
+
+#USE_PAGER = True
+
+#DIRECT_TEMPLATES = ( 'index', )
--- shackspace.de/publishconf.py
+++ shackspace.de/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://shackspace.de'
+SITEURL = '/v2'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
