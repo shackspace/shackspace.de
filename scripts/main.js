69b4e6e8213a212c4d06cb70c0b80fa34bad8256
rashfael <rashfael@isobeef.org>
2015-03-28 19:53:28 +0100

    update upstream

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+(function() {
+  $('#log').slick({
+    infinite: true,
+    slidesToShow: 4,
+    speed: 5000,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 0,
+    pauseOnHover: false,
+    arrows: false,
+    cssEase: 'linear'
+  });
+
+  $.getJSON('http://api.tumblr.com/v2/blog/log.shackspace.de/posts/photo?api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4&callback=?', {}, function(res) {
+    var photo, post, _i, _j, _len, _len1, _ref, _ref1;
+    _ref = res.response.posts;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      post = _ref[_i];
+      _ref1 = post.photos;
+      for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+        photo = _ref1[_j];
+        $('#log').slick('slickAdd', "<div class='slide'><div class='img' style='background-image:url(" + photo.original_size.url + ")'></div></div>");
+      }
+    }
+    return $('#log').slick('slickPlay');
+  });
+
+  window.setDoorStatus = function(status) {
+    var $status;
+    $status = $('#door-status');
+    $status.removeClass();
+    switch (status) {
+      case 'open':
+        return $status.addClass('status-open');
+      case 'closed':
+        return $status.addClass('status-closed');
+      default:
+        return $status.addClass('status-undefined');
+    }
+  };
+
+  $.get('http://shackspace.de/sopen/text/en', {}, function(res) {
+    console.log('the shackspace is ' + res);
+    return window.setDoorStatus(res.trim());
+  });
+
+}).call(this);
+
+ //# sourceMappingURL=main.js.map
