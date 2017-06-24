a6edbe3f9ce53fc242e04d902a66d2bfe5f3da42
rashfael <rashfael@isobeef.org>
2017-06-25 01:02:47 +0200

    add various features

--- blog/themes/shacktheme/source/js/frontpage.js
+++ blog/themes/shacktheme/source/js/frontpage.js
@@ -16,7 +16,6 @@ const SPACE_ACTIVITIES = [
 ]
 
 let activityCounter = 0
-
 const transition = function () {
 	const container = document.querySelector('#activities')
 	container.children[0].classList.add('transition-out')
@@ -31,6 +30,56 @@ const transition = function () {
 	}, 2000)
 }
 
+
+const setDoorStatus = function () {
+	const statusEl = document.querySelector('#status')
+	fetch('https://api.shackspace.de/v1/space').then((response) => {
+		return response.json()
+	}).then((response) => {
+		const status = response.doorState.open
+		if (status) {
+			statusEl.classList.add('open')
+		} else {
+			statusEl.classList.add('closed')
+		}
+	})
+}
+
+const loadLogImages = function () {
+	const container = document.querySelector('#log-container')
+	fetch('https://api.tumblr.com/v2/blog/log.shackspace.de/posts/photo?api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4').then((response) => {
+		return response.json()
+	}).then((response) => {
+		for (const post of response.response.posts) {
+			for (const photo of post.photos) {
+				for (const size of photo.alt_sizes) {
+					if (size.width <= 540) {
+						console.log(size)
+						const img = document.createElement('div')
+						img.classList.add('log-image')
+						img.style['background-image'] = `url(${size.url})`
+						container.appendChild(img)
+						break
+					}
+				}
+			}
+		}
+	})
+		// for post in res.response.posts
+			// for photo in post.photos
+			// 	$('#log').slick 'slickAdd', "<div class='slide'><div class='img' style='background-image:url(#{photo.original_size.url})'></div></div>"
+}
+
 ready(function () {
 	transition()
+	const hero = document.querySelector('.hero')
+	const navbar = document.querySelector('nav.primary')
+	navbar.classList.add('hidden')
+	window.addEventListener('scroll', function(event) {
+		const dim = hero.getBoundingClientRect()
+		if (dim.bottom < 0)
+			navbar.classList.remove('hidden')
+	})
+	setDoorStatus()
+	loadLogImages()
 })
